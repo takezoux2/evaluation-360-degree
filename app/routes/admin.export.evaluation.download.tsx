b1b0c87b0402343ab25b153c989c70d57fe84315
Yoshiteru Takeshita <yoshiteru.takeshita@leverages.jp>
2023-10-02 10:54:54 +0900

    データ出力を修正

--- app/routes/admin.export.evaluation.download.tsx
+++ app/routes/admin.export.evaluation.download.tsx
@@ -34,8 +34,12 @@ export async function action({ params, request }: LoaderArgs) {
       ...term.askSections.flatMap((section) =>
         section.askItems.map((a) => a.askText)
       ),
+      ...term.askSections.flatMap((section) =>
+        section.askItems.map((a) => "自信なし:" + a.askText)
+      ),
     ],
   ];
+  const noConfidenceIndexOffset = askItemIdToColIndex.size;
   for (const evaluation of allEvaluations) {
     const row = new Array(rows[2].length);
     row[0] = evaluation.evaluatee.name;
@@ -44,6 +48,9 @@ export async function action({ params, request }: LoaderArgs) {
     row[3] = evaluation.evaluator.name;
     for (const answer of evaluation.answerItems) {
       row[askItemIdToColIndex.get(answer.askItemId)!] = answer.value;
+      row[
+        askItemIdToColIndex.get(answer.askItemId)! + noConfidenceIndexOffset
+      ] = answer.noConfidence ? "1" : "0";
     }
     rows.push(row);
   }
