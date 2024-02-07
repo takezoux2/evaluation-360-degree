ccd6f25ed6293ecf0b5a82f818ff6e8b25ed0102
takezoux2 <takezoux2@gmail.com>
2024-02-08 02:22:54 +0900

    デザイン調整

--- app/routes/term.$termId/route.tsx
+++ app/routes/term.$termId/route.tsx
@@ -3,7 +3,7 @@ import { useLoaderData } from "@remix-run/react";
 import { getUser, requireUser } from "~/session.server";
 import { TermPanel } from "./TermPanel";
 import { getTermData } from "./effect.server";
-import { toUntil } from "~/time_util";
+import { toHumanFriendly, toUntil } from "~/time_util";
 
 export const meta: V2_MetaFunction = () => [{ title: "アンケートシステム" }];
 
@@ -18,10 +18,12 @@ export default function Index() {
   const { user, termData } = useLoaderData<typeof loader>();
   const buttonClassName =
     "bg-green-300 border border-black p-2 rounded-md w-64";
+
+  const clickableItem = "block rounded border p-2 hover:bg-orange-300";
   const essayExams = termData.essayExams.map((e) => {
     return (
       <div key={e.id}>
-        <a className={buttonClassName} href={`/essay_exam/${e.id}`}>
+        <a className={clickableItem} href={`/essay_exam/${e.id}`}>
           {e.name}
         </a>
       </div>
@@ -35,13 +37,9 @@ export default function Index() {
         <div>状態:{e.state}</div>
         <div>
           {e.state === "回答済" ? (
-            <span
-              className={buttonClassName.replace("bg-green-300", "bg-gray-300")}
-            >
-              回答済
-            </span>
+            <span className="block rounded border bg-gray-300 p-2">回答済</span>
           ) : (
-            <a href={`/exam/${e.id}`} className={buttonClassName}>
+            <a href={`/exam/${e.id}`} className={clickableItem}>
               回答する
             </a>
           )}
@@ -49,56 +47,57 @@ export default function Index() {
       </div>
     );
   });
+  const pHeader = "bold rounded-t-md bg-indigo-300 p-2 text-lg";
   return (
     <main className="relative flex min-h-screen flex-col bg-white p-2">
       <div className="flex flex-col rounded-md bg-blue-100">
-        <div className="rounded-t-md bg-blue-300 p-2">受験期間情報</div>
+        <div className="rounded-t-md bg-blue-300 p-2 text-lg font-bold">
+          受験期間情報
+        </div>
 
-        <div className="flex flex-col p-2">
+        <div className="flex flex-col">
           <div className="flex flex-row">
-            <div className="w-32 border border-black px-3 text-right">名前</div>
-            <div className="w-64 border border-black  px-2">
+            <div className="w-32 border border-gray-500 px-3 py-1 text-right">
+              名前
+            </div>
+            <div className="w-full border border-gray-500 px-3 py-1">
               {termData.term.name}
             </div>
           </div>
           <div className="flex flex-row">
-            <div className=" w-32 border border-black px-3 text-right">
+            <div className="w-32 border border-gray-500 px-3 py-1 text-right">
               受験期間
             </div>
-            <div className="w-64 border border-black px-2">
-              ～{toUntil(new Date(termData.term.endAt))}まで
+            <div className="w-full border border-gray-500 px-3 py-1">
+              {toHumanFriendly(termData.term.startAt)}～
+              {toUntil(new Date(termData.term.endAt))}まで
             </div>
           </div>
         </div>
       </div>
 
       <div className="mt-3 flex flex-col rounded-md bg-indigo-100">
-        <div className="rounded-t-md bg-indigo-300 p-2">180度評価</div>
-        <div className="flex flex-col p-2">
-          <div>
-            <a
-              className={buttonClassName}
-              href={`/evaluation/${termData.term.id}`}
-            >
-              評価する
-            </a>
-          </div>
+        <div className={pHeader}>180度評価</div>
+        <div className="flex flex-col">
+          <a className={clickableItem} href={`/evaluation/${termData.term.id}`}>
+            評価開始
+          </a>
         </div>
       </div>
       <div className="mt-3 flex flex-col rounded-md bg-indigo-100">
-        <div className="rounded-t-md bg-indigo-300 p-2">スキルテスト</div>
-        <div className="flex flex-col p-2">{exams}</div>
+        <div className={pHeader}>スキルテスト</div>
+        <div className="flex flex-col">{exams}</div>
       </div>
       <div className="mt-3 flex flex-col rounded-md bg-indigo-100">
-        <div className="rounded-t-md bg-indigo-300 p-2">記述試験</div>
-        <div className="flex flex-col p-2">{essayExams}</div>
+        <div className={pHeader}>記述試験</div>
+        <div className="flex flex-col">{essayExams}</div>
       </div>
       <div className="mt-3 flex flex-col rounded-md bg-indigo-100">
-        <div className="rounded-t-md bg-indigo-300 p-2">スキルシート入力</div>
-        <div className="flex flex-col p-2">
+        <div className={pHeader}>スキルシート入力</div>
+        <div className="flex flex-col">
           <div>
             <a
-              className={buttonClassName}
+              className={clickableItem}
               href={`/skill_sheet/${termData.term.id}`}
             >
               入力
